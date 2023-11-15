e4a73a5f537d5977f0266be1844e49fcecc4cea4
ted-miller92 <ted.miller92@gmail.com>
2023-11-15 14:31:47 -0800

    added ddl and dml files

---
+++ DDL.sql
@@ -0,0 +1,162 @@
+-- Authors: Ted Miller, Chris Garrett
+-- Date: 10/26/2023
+
+USE music_streamer;
+
+-- Drop tables before creating new ones
+DROP TABLE IF EXISTS Users, Artists, Release_Types, Genres, Releases, Songs, Song_Artists, Playlists, Playlist_Songs;
+
+-- Users table
+CREATE TABLE Users(
+	user_id INT NOT NULL AUTO_INCREMENT,
+    user_name VARCHAR(255),
+    user_email VARCHAR(255),
+    PRIMARY KEY (user_id)
+);
+
+-- Artists table
+CREATE TABLE Artists(
+	artist_id INT NOT NULL AUTO_INCREMENT,
+    artist_name VARCHAR(255) NOT NULL,
+    artist_description MEDIUMTEXT,
+    PRIMARY KEY (artist_id)
+);
+
+-- Release_Types table (album, extended-play, single, etc.)
+CREATE TABLE Release_Types(
+	release_type_id INT NOT NULL AUTO_INCREMENT,
+    release_type_name VARCHAR(255) NOT NULL,
+    PRIMARY KEY (release_type_id)
+);
+
+-- Genres table
+CREATE TABLE Genres(
+	genre_id INT NOT NULL AUTO_INCREMENT,
+    genre_name VARCHAR(255) NOT NULL,
+    PRIMARY KEY (genre_id)
+);
+
+-- Releases table
+-- References Release_Types to identify release as an album, ep, single, etc.
+-- References Artists to identify who published/released the album, ep, etc.
+CREATE TABLE Releases(
+	release_id INT NOT NULL AUTO_INCREMENT,
+    release_name VARCHAR(255) NOT NULL,
+    release_type_id INT,
+    artist_id INT NOT NULL,
+    PRIMARY KEY (release_id),
+    FOREIGN KEY (release_type_id) REFERENCES Release_Types(release_type_id) 
+		ON DELETE SET NULL	-- When a Release_Type is deleted, this field will be set to Null
+        ON UPDATE CASCADE,	-- When a Release_Type is updated, this will cascade to be reflected in this table
+    FOREIGN KEY (artist_id) REFERENCES Artists(artist_id) 
+		ON DELETE CASCADE	-- When an Artist is deleted, all of their Releases will also be deleted
+        ON UPDATE CASCADE	-- When an Artist is updated, all of their Releases will also be updated
+);
+
+-- Songs table
+-- References Releases to attribute it to a specific release
+-- References Genres to classify it as a certain genre
+CREATE TABLE Songs(
+	song_id INT NOT NULL AUTO_INCREMENT,
+    song_name VARCHAR(255) NOT NULL,
+    release_id INT NOT NULL,
+    genre_id INT NULL,
+    stream_count INT NOT NULL,
+    PRIMARY KEY (song_id),
+    FOREIGN KEY (release_id) REFERENCES Releases(release_id) 
+		ON DELETE CASCADE,	-- When a Release is deleted, the associated songs will be deleted
+    FOREIGN KEY (genre_id) REFERENCES Genres(genre_id) 
+		ON DELETE SET NULL	-- When a genre is deleted, this field will be set to Null
+);
+
+-- Song_Artists table. This is an intersection table that
+-- keeps track of the Artist(s) attributed to a song
+CREATE TABLE Song_Artists(
+	song_artists_id INT NOT NULL AUTO_INCREMENT,
+    song_id INT NOT NULL,
+    artist_id INT NOT NULL,
+    PRIMARY KEY (song_artists_id),
+    FOREIGN KEY (song_id) REFERENCES Songs(song_id) 
+		ON DELETE CASCADE,	-- When a Song is deleted, the Song_Artist record will also be deleted
+    FOREIGN KEY (artist_id) REFERENCES Artists(artist_id) 
+		ON DELETE CASCADE	-- When an Artist is deleted, the Song_Artist record will also be deleted
+);
+
+-- Playlists table
+CREATE TABLE Playlists(
+	playlist_id INT NOT NULL AUTO_INCREMENT,
+    playlist_name VARCHAR(255) NOT NULL,
+    user_id INT NOT NULL,
+    PRIMARY KEY (playlist_id),
+    FOREIGN KEY (user_id) REFERENCES Users(user_id) 
+		ON DELETE CASCADE	-- When a User is deleted, their playlists will also be deleted
+);
+
+-- Playlist_Songs table. Intersection table that tracks
+-- the songs that are on a playlist
+CREATE TABLE Playlist_Songs(
+	playlist_song_id INT NOT NULL AUTO_INCREMENT,
+    playlist_id INT NOT NULL,
+    song_id INT NOT NULL,
+    PRIMARY KEY (playlist_song_id),
+    FOREiGN KEY (playlist_id) REFERENCES Playlists(playlist_id) 
+		ON DELETE CASCADE,	-- When a Playlist is deleted, all records associated in this table will be deleted
+    FOREIGN KEY (song_id) REFERENCES Songs(song_id)
+		ON DELETE CASCADE	-- When a Song is deleted, all records associated in this table will be deleted
+);
+
+
+INSERT INTO Genres(genre_name) 
+VALUES ("Rock"), ("Alternative Rock"), ("Pop"), ("Electronic"), ("Country");
+
+INSERT INTO Release_Types(release_type_name) 
+VALUES ("Single"), ("Album"), ("EP"), ("Collaboration");
+
+INSERT INTO Artists(artist_name, artist_description) 
+VALUES 
+	("Linkin Park", "Band Members: Chester Bennington, Mike Shinoda, Brad Delson, Dave Farrell, Joe Hahn, Rob Bourdon"),
+	("Red Hot Chili Peppers", "Band Members: Anthony Kiedis, Chad Smith, Michael Balzary, John Frusciante"),
+	("Queen", "Band Members: Freddie Mercury, Brian May, Roger Taylor, John Deacon");
+
+INSERT INTO Releases(release_name, release_type_id, artist_id) 
+VALUES 
+	("Meteora", (SELECT release_type_id FROM Release_Types WHERE release_type_name = "Album"), (SELECT artist_id FROM Artists WHERE artist_name = "Linkin Park")),
+	("By The Way", (SELECT release_type_id FROM Release_Types WHERE release_type_name = "Album"), (SELECT artist_id FROM Artists WHERE artist_name = "Red Hot Chili Peppers")),
+	("A Night at the Opera", (SELECT release_type_id FROM Release_Types WHERE release_type_name = "Album"), (SELECT artist_id FROM Artists WHERE artist_name = "Queen"));
+
+INSERT INTO Songs(song_name, release_id, genre_id, stream_count)
+VALUES
+	("Breaking the Habit", (SELECT release_id FROM Releases WHERE release_name = "Meteora"), (SELECT genre_id FROM Genres WHERE genre_name = "Alternative Rock"), 0),
+	("Can't Stop", (SELECT release_id FROM Releases WHERE release_name = "By The Way"), (SELECT genre_id FROM Genres WHERE genre_name = "Alternative Rock"), 0),
+	("By The Way", (SELECT release_id FROM Releases WHERE release_name = "By The Way"), (SELECT genre_id FROM Genres WHERE genre_name = "Alternative Rock"), 0),
+	("Bohemian Rhapsody", (SELECT release_id FROM Releases WHERE release_name = "A Night at the Opera"), (SELECT genre_id FROM Genres WHERE genre_name = "Rock"), 0),
+	("Somewhere I Belong", (SELECT release_id FROM Releases WHERE release_name = "Meteora"), (SELECT genre_id FROM Genres WHERE genre_name = "Alternative Rock"), 0);
+
+INSERT INTO Song_Artists(song_id, artist_id)
+VALUES
+	((SELECT song_id FROM Songs WHERE song_name = "Breaking the Habit"), (SELECT artist_id FROM Artists WHERE artist_name = "Linkin Park")),
+	((SELECT song_id FROM Songs WHERE song_name = "Can't Stop"), (SELECT artist_id FROM Artists WHERE artist_name = "Red Hot Chili Peppers")),
+	((SELECT song_id FROM Songs WHERE song_name = "By The Way"), (SELECT artist_id FROM Artists WHERE artist_name = "Red Hot Chili Peppers")),
+	((SELECT song_id FROM Songs WHERE song_name = "Bohemian Rhapsody"), (SELECT artist_id FROM Artists WHERE artist_name = "Queen")),
+	((SELECT song_id FROM Songs WHERE song_name = "Somewhere I Belong"), (SELECT artist_id FROM Artists WHERE artist_name = "Linkin Park"));
+
+INSERT INTO Users (user_name, user_email)
+VALUES 
+	("Ted Miller", "millert8@oregonstate.edu"), 
+    ("Chris Garrett", "garrchri@oregonstate.edu"), 
+    ("John Smith", "JohnSmith@fakeuser.com");
+
+INSERT INTO Playlists(playlist_name, user_id)
+VALUES
+	("playlist 1", (SELECT user_id FROM Users WHERE user_name = "Ted Miller")),
+	("playlist 2", (SELECT user_id FROM Users WHERE user_name = "Chris Garrett")),
+	("playlist 3", (SELECT user_id FROM Users WHERE user_name = "John Smith"));
+
+INSERT INTO Playlist_Songs(playlist_id, song_id)
+VALUES
+	((SELECT playlist_id FROM Playlists WHERE playlist_name = "playlist 1"), (SELECT song_id FROM Songs WHERE song_name = "Breaking the Habit")),
+	((SELECT playlist_id FROM Playlists WHERE playlist_name = "playlist 1"), (SELECT song_id FROM Songs WHERE song_name = "Can't Stop")),
+	((SELECT playlist_id FROM Playlists WHERE playlist_name = "playlist 2"), (SELECT song_id FROM Songs WHERE song_name = "Somewhere I Belong")),
+	((SELECT playlist_id FROM Playlists WHERE playlist_name = "playlist 2"), (SELECT song_id FROM Songs WHERE song_name = "Bohemian Rhapsody")),
+	((SELECT playlist_id FROM Playlists WHERE playlist_name = "playlist 3"), (SELECT song_id FROM Songs WHERE song_name = "Breaking the Habit")),
+	((SELECT playlist_id FROM Playlists WHERE playlist_name = "playlist 3"), (SELECT song_id FROM Songs WHERE song_name = "By The Way"));
---
+++ DML.sql
@@ -0,0 +1,239 @@
+/* 
+Queries that are to be commonly used in
+the implementation of the website and querying of the database.
+Dynamically generated variables denoted by ${variableName}
+*/
+
+USE music_streamer;
+
+/* Artists
+The following are queries to be used for basic CRUD operations
+on the Artists table. 
+*/
+
+-- Fetch Artists, 
+SELECT * FROM Artists;
+
+-- Create an artist
+INSERT INTO Artists(artist_name, artist_description)
+VALUES(${artistName}, ${artistDescription})
+
+-- Update artist info
+UPDATE Artists
+SET artist_name = "${artistName}"
+WHERE artist_id = ${artist_id};
+
+UPDATE Artists
+SET artist_description = ${artist_description}
+WHERE artist_id = ${artist_id};
+
+-- Delete an artist
+DELETE FROM Artists
+WHERE artist_id = ${artist_id};
+
+/* Genres
+The following are queries to be used for basic CRUD operations
+on the Genres table. 
+*/
+
+-- Fetch Genres
+SELECT * FROM Genres;
+
+SELECT genre_id FROM Genres WHERE genre_name = "${genreName}";
+
+-- Create Genres
+INSERT INTO Genres(genre_name)
+VALUES(${genreName});
+
+-- Update Genres
+UPDATE Genres
+SET genre_name = "${genreName}"
+WHERE genre_id =${genreID};
+
+-- Delete Genres
+DELETE FROM Genres
+WHERE genre_id =${genreID};
+
+/* Playlists
+The following are queries to be used for basic CRUD operations
+on the Playlists table. 
+*/
+
+-- Fetch playlists
+SELECT * FROM Playlists;
+
+SELECT * FROM Playlists
+WHERE playlist.user_id = ${userID};
+
+-- Create a new playlist
+INSERT INTO Playlists(playlist_name, user_id)
+VALUES(${playlistName}, ${userID});
+
+-- Update a playlist
+UPDATE Playlists
+SET playlist_name = "${playlistName}"
+WHERE playlist_id = ${playlistID};
+
+-- Delete a playlist
+DELETE FROM Playlists
+WHERE playlist_id = ${playlistID};
+
+/* Playlist_Songs
+The following are queries to be used for basic CRUD operations
+on the Playlist_Songs table. 
+*/
+
+-- Fetch Playlist_Songs of a given Playlist
+SELECT song_id FROM Playlist_Songs
+WHERE Playlist_Songs.playlist_id = ${playlistID};
+
+-- Create a new Playlist_Songs record 
+-- That is, add a song to playlist
+INSERT INTO Playlist_Songs(playlist_id, song_id)
+VALUES(${playlistID}, ${songID});
+
+-- Update a Playlist_Songs record
+-- Unlikely to be necessary, as the more likely
+-- work flow will be to remove a song and add a new one
+UPDATE Playlist_Songs
+SET playlist_id = ${playlistID}
+WHERE playlist_song_id = ${playlistSongID};
+
+-- Delete a Playlist_Songs record
+-- That is, remove a song from a Playlist
+DELETE FROM Playlist_Songs
+WHERE playlist_id = ${playlistID}
+AND song_id = ${songID};
+
+/* Releases
+The following are queries to be used for basic CRUD operations
+on the Releases table. 
+*/
+
+-- Fetch releases of a given artist
+SELECT * FROM Releases
+WHERE artist_id = ${artistID};
+
+-- Create a new release
+INSERT INTO Releases(release_name, release_type_id, artist_id)
+VALUES("${releaseName}", ${releaseTypeID}, ${artistID});
+
+-- Update a release
+UPDATE Releases
+SET release_name = "${releaseName}"
+WHERE release_id = ${releaseID};
+
+UPDATE Releases
+SET release_type_id = ${releaseTypeID}
+WHERE release_id = ${releaseID};
+
+-- Delete a release
+DELETE FROM Releases
+WHERE release_id = ${releaseID};
+
+/* Release_Types
+The following are queries to be used for basic CRUD operations
+on the Release_Types table. 
+*/
+
+-- Fetch release types
+SELECT * FROM Release_Types;
+
+SELECT release_type_id FROM Release_Types;
+
+-- Create a new Release_Type
+INSERT INTO Release_Types(release_type_name)
+VALUES("${releaseTypeName}");
+
+-- Update a Release_Type
+UPDATE Release_Types
+SET release_type_name = "${releaseTypeName}"
+WHERE release_type_id = ${releaseTypeID};
+
+-- Delete a Release_Type
+DELETE FROM Release_Types
+WHERE release_type_id = ${releaseTypeID};
+
+/* Songs
+The following are queries to be used for basic CRUD operations
+on the Songs table. 
+*/
+
+-- Fetch all songs from a Release
+SELECT * FROM Songs
+WHERE release_id = ${releaseID};
+
+-- Fetch all songs from an Artist
+SELECT * FROM Songs
+INNER JOIN Song_Artists ON Songs.song_id = Song_Artists.song_id
+WHERE Song_Artists.artist_id = ${artistID};
+
+-- Create a new song
+INSERT INTO Songs(song_name, release_id, genre_id, stream_count)
+VALUES("${songName}", ${releaseID}, ${genreID}, 0);
+
+-- Update a song
+UPDATE Songs
+SET song_name = "${songName}"
+WHERE song_id = ${songId};
+
+UPDATE Songs
+SET genre_id = ${genreID}
+WHERE song_id = ${songID};
+
+-- Delete a song
+DELETE FROM Songs
+WHERE song_id = ${songID};
+
+/* Song_Artists
+The following are queries to be used for basic CRUD operations
+on the Song_Artists table. Basically, this will be attributing one or more
+artists to any given song
+*/
+
+-- Select all songs from a given artist
+SELECT * FROM Song_Artists
+WHERE Song_Artists.artist_id = ${artistID};
+
+-- Select the artist(s) of a song
+SELECT artist_id FROM Song_Artists
+WHERE Song_Artists.song_id = ${songID};
+
+-- Insert into Song_Artists
+-- basically, attribute a song to an artist
+INSERT INTO Song_Artists(song_id, artist_id)
+VALUES (${songID}, ${artistID});
+
+-- Delete from Song_Artists
+DELETE FROM Song_Artists
+WHERE song_artists_id = ${songArtistsID};
+
+/* Users
+The following are queries to be used for basic CRUD operations
+on the Users table. 
+*/
+
+-- Select all users
+SELECT * FROM Users;
+
+-- Select a specific user
+SELECT * FROM Users
+WHERE Users.user_email = "${userEmail}";
+
+-- Add a new user
+INSERT INTO Users(user_name, user_email)
+VALUES("${userName}", "${user_email}");
+
+-- Update a user's email
+UPDATE USERS
+SET user_email = ${userEmail}
+WHERE user_id = ${userID};
+
+-- Update a user's username
+UPDATE USERS
+SET user_name = ${userName}
+WHERE user_id = ${userID};
+
+-- Delete a user
+DELETE FROM Users
+WHERE user_id = ${userID};
